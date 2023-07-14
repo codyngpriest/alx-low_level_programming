fc7175e50c21e89710468d00427f87cc5cf46e98
codyngpriest <vilhopriestly@gmail.com>
2023-07-14 21:55:10 +0000

    updated 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,15 +18,15 @@ printf("%d", n);
 
 if (n > 0)
 {
-printf(" is positive \n");
+printf(" is positive\n");
 }
 else if (n < 0)
 {
-printf(" is negative \n");
+printf(" is negative\n");
 }
 else
 {
-printf(" is zero \n");
+printf(" is zero\n");
 }
 return (0);
 }
