21d8681dcc6fee81af97a1ab99052606c012b039
codyngpriest <vilhopriestly@gmail.com>
2023-07-19 07:12:01 +0000

    created 0-main.c, main.h

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,3 @@
+0x03. C - Debugging
+0. Multiple mains
+# This is a 0-main.c file that test the function positive_or_negative() to give the correct output when given a case of 0.
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/* checks if an integer is positive or negative */
+void positive_or_nagative(int i);
+
+#endif /* MAIN_H */
