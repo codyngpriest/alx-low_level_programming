eb9064635617bd2d3d7c9697632ba597f514a983
codyngpriest <vilhopriestly@gmail.com>
2023-07-20 20:56:06 +0000

    created 3-print_remaining_days.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -9,4 +9,7 @@ void positive_or_nagative(int i);
 /* prints the largest of three numbers */
 int largest_number(int a, int b, int c);
 
+/* prints remaining days */
+void print_remaining_days(int month, int day, int year);
+
 #endif /* MAIN_H */
