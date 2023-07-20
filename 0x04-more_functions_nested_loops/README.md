88205e6498b3ebcdd4e1cd3a76306e553a4448a9
codyngpriest <vilhopriestly@gmail.com>
2023-07-20 07:09:20 +0000

    created 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+0x04. C - More functions, more nested loops
+0. isupper
+# This is a function that checks for uppercase character.
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+
+/* prints a charater to stdout */
+int _putchar(char c);
+
+/* checks for uppercase character */
+int _isupper(int c);
+
+#endif /* MAIN_H */
