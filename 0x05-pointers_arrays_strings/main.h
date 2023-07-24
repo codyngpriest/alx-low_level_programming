b1a5d7c30511c83e60399ef22efdbe54a3bb783d
codyngpriest <vilhopriestly@gmail.com>
2023-07-24 07:10:01 +0000

    created 2-strlen.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -12,5 +12,7 @@ void reset_to_98(int *n);
 /* swaps the values of two integers */
 void swap_int(int *a, int *b);
 
+/* returns the length of a string */
+int _strlen(char *s);
 
 #endif /* MAIN_H */
