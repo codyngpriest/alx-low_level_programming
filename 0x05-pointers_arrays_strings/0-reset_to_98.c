ecaa4673ca80b934eaff5b37cf072c92d1d9a3ff
codyngpriest <vilhopriestly@gmail.com>
2023-07-24 06:44:07 +0000

    created 0-reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+/**
+* reset_to_98 - takes a pointer to an int as a parameter and updates the value
+* it points to to 98
+*
+* @n: a poniter to the value
+*
+* Return: Always 0 (Success)
+*/
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+
+/* prints a single character to stdout */
+int _putchar(char c);
+
+/* takes a pointer of an int and updates the value it points to 98 */
+void reset_to_98(int *n);
+
+
+#endif /* MAIN_H */
