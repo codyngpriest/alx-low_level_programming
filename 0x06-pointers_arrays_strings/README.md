bf549011655bd7ff8b4b50dd9b7691d9db1791fc
codyngpriest <vilhopriestly@gmail.com>
2023-07-26 07:43:57 +0000

    created 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+0x06. C - More pointers, arrays and strings
+0. strcat
+# This is a function that concatenates two strings.
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+
+/* prints a single character to stdout */
+int _putchar(char c);
+
+/* concatenates two strings */
+char *_strcat(char *dest, char *src);
+
+
+
+#endif /* MAIN_H */
