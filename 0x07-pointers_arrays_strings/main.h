b7811b3e7dd639c1ae5764261286abbe40dc66ad
codyngpriest <vilhopriestly@gmail.com>
2023-07-31 07:04:58 +0000

    created 0-memset.c

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+/* prints a single charater to stdout */
+int _putchar(char c);
+
+/* feels memory with a constant byte */
+char *_memset(char *s, char b, unsigned int n);
+
+
+
+#endif /* MAIN_H */
