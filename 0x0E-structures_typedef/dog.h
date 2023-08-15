57032a200ae2e09e603c272a47dbfaf4df076d46
codyngpriest <vilhopriestly@gmail.com>
2023-08-15 19:10:15 +0000

    created dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+/**
+ * struct dog - defines information about a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
+/* Structure definition for a dog */
+struct dog
+{
+char *name;     /* Name of the dog */
+float age;      /* Age of the dog */
+char *owner;    /* Owner of the dog */
+};
+
+#endif /* MAIN_H */
