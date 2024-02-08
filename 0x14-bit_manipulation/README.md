fa23ecc942d853ac270c07314c99ac5c9c61b787
codyngpriest <vilhopriestly@gmail.com>
2024-02-08 09:14:58 +0000

    create function to convert binary to unsigned int

---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,4 @@
+0x14. C - Bit manipulation
+
+0. 0
+This is a function that converts a binary number to an unsigned int.
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stddef.h>
+
+/* Function prototypes */
+
+/* converts a binary number to an unsigned int */
+unsigned int binary_to_uint(const char *b);
+int _putchar(char c);
+
+
+#endif
